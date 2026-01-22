1886c613858387dcf1c5ca45693078a94d7727c5
mikuwithbeer <git@miku.beer>
2026-01-22 03:56:04 +0300

    feat: better cli handling

--- src/main.zig
+++ src/main.zig
@@ -15,43 +15,60 @@ pub fn main() !void {
     const args = try std.process.argsAlloc(allocator);
     defer std.process.argsFree(allocator, args);
 
-    if (args.len < 3) {
-        std.debug.print("usage: llim run|asm <file>\n", .{});
+    if (args.len != 4) {
+        std.debug.print(
+            \\usage:
+            \\  llim compile <source> <target>
+            \\  llim execute bytecode <target>
+            \\
+        , .{});
         return;
     }
 
-    const mode = args[1];
-    const file = args[2];
+    const firstly = args[1];
+    const secondary = args[2];
+    const target = args[3];
 
-    var bytecode = try Bytecode.fromFile(allocator, file);
+    if (std.mem.eql(u8, firstly, "compile")) {
+        try compileFile(allocator, secondary, target);
+    } else if (std.mem.eql(u8, firstly, "execute") and std.mem.eql(u8, secondary, "bytecode")) {
+        try executeBytecodeFile(allocator, target);
+    } else {
+        unknownMode();
+    }
+}
+
+fn compileFile(allocator: std.mem.Allocator, source: []const u8, target: []const u8) !void {
+    var tokens: std.ArrayList(Token) = .empty;
+    defer tokens.deinit(allocator);
+
+    var lexer = try Lexer.init(allocator, source, &tokens);
+    defer lexer.deinit();
+
+    try lexer.loop();
+
+    var assembler = try Assembler.init(allocator, &tokens);
+    defer assembler.deinit();
+
+    try assembler.prepare();
+    try assembler.loop();
+
+    try assembler.writeFile(target);
+
+    std.debug.print("wrote {d} bytes.\n", .{assembler.bytes});
+}
+
+fn executeBytecodeFile(allocator: std.mem.Allocator, target: []const u8) !void {
+    var bytecode = try Bytecode.fromFile(allocator, target);
     defer bytecode.deinit();
 
-    if (std.mem.eql(u8, mode, "run")) {
-        var machine = try Machine.init(allocator, bytecode);
-        defer machine.deinit();
+    var machine = try Machine.init(allocator, bytecode);
+    defer machine.deinit();
 
-        machine.setPermission(.Write);
-        try machine.loop();
-    } else if (std.mem.eql(u8, mode, "asm")) {
-        var tokens: std.ArrayList(Token) = .empty;
-        defer tokens.deinit(allocator);
+    machine.setPermission(.Write);
+    try machine.loop();
+}
 
-        var lexer = try Lexer.init(allocator, file, &tokens);
-        defer lexer.deinit();
-
-        try lexer.loop();
-
-        var assembler = try Assembler.init(allocator, &tokens);
-        defer assembler.deinit();
-
-        try assembler.prepare();
-        try assembler.loop();
-
-        for (assembler.bytecode.values.items) |byte| {
-            std.debug.print("0x{X} ", .{byte});
-        }
-        std.debug.print("\n", .{});
-    } else {
-        std.debug.print("unknown mode: {s}\n", .{mode});
-    }
+fn unknownMode() void {
+    std.debug.print("unknown mode?\n", .{});
 }
